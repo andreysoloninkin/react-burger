0b5684c8bf8f54321b2bc0c3baaef44c7334d8d9
Андрей Солонинкин <andreysoloninkin@yandex.ru>
2022-02-13 01:00:02 +0300

    Исправления

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -12,47 +12,10 @@ import { HTML5Backend } from 'react-dnd-html5-backend';
 import { DndProvider } from 'react-dnd';
 import { SET_INGREDIENTS } from '../../services/actions';
 
-const ingredientsInitialState = [];
-
-const appInitalState = { 
-  tab: "bun",
-  ingredients: ingredientsInitialState,
-  order: [], //jsData.order,
-  total: 0,
-  orderid: "0"
-};
-
-function changeState(state, action){
-  let total = 0;
-  const items = state.order;
-  if(items.length>0){
-    items.filter((item)=>(item.type!=="bun")).forEach(element => {
-      total+=element.price;
-    });
-    total += 2 * items.filter((item)=>(item.type==="bun"))[0].price;
-  }
-
-  switch (action.type)
-  {
-    case "init": 
-        return {...state, ingredients:action.data, total: total};
-    case "order":
-        return {...state, total: total};
-    case "orderid":
-          return {...state, orderid: action.data.order.number};
-    default:
-        return {...state};
-  }
-}
 
 function App(){
 
   const dispatch = useDispatch();
-
-  ///*
-  const appState = React.useReducer(changeState, appInitalState)
-  ///*
-  const [state, stateDispatch] = appState;
 
   React.useEffect(
     ()=>
@@ -62,8 +25,6 @@ function App(){
           const res = await fetch(API_PATH+"ingredients");
           if (res.ok) {
             const json = await res.json();
-            ///*
-            stateDispatch({type:"init", data:json.data}); 
             dispatch({type: SET_INGREDIENTS, data:json.data});
           }else{
             console.log('APP_ERROR:',res.statusText);
@@ -77,8 +38,6 @@ function App(){
     []
   );
   
-  //<AppContext.Provider value={appState}>
-  //</AppContext.Provider>
 
   return (
       <div className={ styles.App }>
